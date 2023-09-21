b2409bb1847571184d6b78715931b159386ddd12
Maciej <msypniewski511@gmail.com>
2023-09-22 00:22:09 +0100

    Change temporarli root path

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < StoreController
+  helper 'spree/products'
+  respond_to :html
+
+  def home
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
-  root to: 'home#index'
+  root to: 'pages#home'
 
   devise_for(:user, {
     class_name: 'Spree::User',
@@ -70,9 +70,10 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/store'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "home/index"
 end
