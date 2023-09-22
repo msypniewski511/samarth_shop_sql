20cca2ea21db3d1e3d1549bdce30a9e644b2a11a
Maciej <msypniewski511@gmail.com>
2023-09-22 21:34:38 +0100

    First proposition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,6 @@ class PagesController < StoreController
   def home
     @searcher = build_searcher(params.merge(include_images: true))
     @products = @searcher.retrieve_products
+    @product = Spree::Product.last
   end
 end
