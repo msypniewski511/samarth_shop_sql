94950a94ac47919f42e19b7dfc96a0907a237612
Maciej <msypniewski511@gmail.com>
2023-09-23 21:05:56 +0100

    Remove error in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => <%= Rails.application.credentials.dig(:mail, :user_name) %>,
-    :password             => <%= Rails.application.credentials.dig(:mail, :password) %>,
+    :user_name            => Rails.application.credentials.dig(:mail, :user_name),
+    :password             => Rails.application.credentials.dig(:mail, :password),
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
